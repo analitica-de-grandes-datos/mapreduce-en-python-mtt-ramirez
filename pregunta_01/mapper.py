f70a18a3e28fdc349013b8cfb232239a01be5dcf
mtt-ramirez <112211254+mtt-ramirez@users.noreply.github.com>
2023-06-10 18:22:16 -0500

    intento3

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,13 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+import csv
+
+# Lee los registros del archivo credit.csv y emite los pares clave-valor
+with open('credit.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    
+    for row in reader:
+        credit_history = row[2]  # Obtiene el valor de la tercera columna (credit_history)
+        print(f'{credit_history}\t1')
--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -1,3 +1,25 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+current_state = None
+count = 0
+
+# Lee los pares clave-valor del mapper y realiza la reducción
+for line in sys.stdin:
+    line = line.strip()
+    state, value = line.split('\t', 1)
+    
+    if current_state == state:
+        count += int(value)
+    else:
+        if current_state:
+            # Emite el resultado del estado anterior
+            print(f'{current_state}\t{count}')
+        current_state = state
+        count = int(value)
+
+# Emite el resultado del último estado
+if current_state:
+    print(f'{current_state}\t{count}')
